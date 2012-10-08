b64ef3a8a8e0adb204212abf9758c4a45b0adb6b
Norberto Yoan <norberto.yoan@cymait.com>
2012-10-08 20:24:18 -0300

    Refactor de plugin con iteradores.
    Todavía no funciona.

--- src/autotext/autotext.js
+++ src/autotext/autotext.js
@@ -7,74 +7,106 @@
 		return (str + '').replace(/([^>\r\n]?)(\r\n|\n\r|\r|\n)/g, '$1'+ breakTag);
 	}
 
-	var animations = {
-		char: function(data, content) {
-			if (!content.current_item) {
-				content.current_item = 0;
-			}
-			//content.text = nl2br(content.text);
-			if (data.running && content.current_item < content.text.length) {
-				setTimeout(function()
-					{
-						char = content.text.charAt(content.current_item);
-						if (char == '\r') {
-							// ignoro el \r
-							content.current_item++;
-							char = content.text.charAt(content.current_item);
-						}
-						if (char == '\n') {
-							char = '<br />';
-						}
-						content.placeholder.html(content.placeholder.html() + char);
-						content.current_item++;
-						animations.char(data, content);
-					}, content.delay);
-			}
-		},
-		line: function(data, content) {
-			if (!content.current_item) {
-				content.current_item = 0;
-			}
-			var lines = content.text.match(/^.*((\r\n|\n|\r)|$)/gm);
-			if (data.running && content.current_item < lines.length) {
-				setTimeout(function()
-					{
-						content.placeholder.append(lines[content.current_item]);
-						content.current_item++;
-						animations.line(data, content);
-					}, content.delay);
-			}
-		},
-		one_shot: function(data, content) {
-			if (!content.current_item) {
-				content.current_item = 0;
-			}
-			if (content.current_item = 0 && data.running) {
-				setTimeout(function()
-					{
-						content.placeholder.append(content.text);
-						content.current_item++
-					}, content.delay);
-			}
-		},
-		replace: function(data, content) {
-			if (content.placeholder == data.target) {
-				content.placeholder = target.append('<span>');
-			}
-
-			// convertir el texto en un objeto iterable para todas las animaciones
-			// TODO chequear si el placeholder == taget porque en
-			// ese caso no tiene su propio placeholder.
-			// El de reemplazo tiene que tener su propio porque pisa todo el contenido.
-		}
-	}
-
-	function render_placeholder(content, target) {
+	function create_placeholder(content, target) {
 		if (!content.placeholder) {
 			content.placeholder = target;
 		} else if (typeof content.placeholder == "string") {
 			content.placeholder = $(content.placeholder);
 			target.append(content.placeholder);
+		}
+	}
+
+	function assign_iterator(content) {
+		if (!content.current_iterator) {
+			content.current_iterator = render_strategies[content.render_strategy](content.items);
+		}
+	}
+
+	function line_iterator(text) {
+		this.current_line = 0;
+		this.lines = text.match(/^.*((\r\n|\n|\r)|$)/gm);
+	}
+
+	line_iterator.prototype.has_next = function() {
+		return this.current_line < this.lines.length;
+	}
+
+	line_iterator.prototype.next = function() {
+		return this.lines[this.current_line++];
+	}
+
+	function char_iterator(text) {
+		this.current_char = 0;
+		this.text = text;
+	}
+
+	char_iterator.prototype.has_next = function() {
+		return this.current_char < this.text.length;
+	}
+
+	char_iterator.prototype.next = function() {
+		char = this.text.charAt(this.current_char++);
+		if (char == '\r') {
+			// ignoro el \r
+			char = content.text.charAt(this.current_item++);
+		}
+		if (char == '\n') {
+			char = '<br />';
+		}
+		return char;
+	}
+
+	function one_shot_iterator(text) {
+		this.listed = false;
+		this.text = text;
+	}
+
+	char_iterator.prototype.has_next = function() {
+		return this.listed;
+	}
+
+	char_iterator.prototype.next = function() {
+		this.listed = true;
+		return this.text
+	}
+
+	var animations = {
+		additive: function(data, content) {
+			create_placeholder(content, data.target);
+			assign_iterator(content);
+			if (data.running && content.current_iterator.has_next()) {
+				setTimeout(function() {
+					content.placeholder.html(content.placeholder.html() + content.current_iterator.next());
+					animations.additive(data, content);
+				}, content.delay);
+			}
+		},
+		replace: function(data, content) {
+			// placeholder is necessary
+			if (!content.placeholder || content.placeholder == '') content.placeholder = '<span>';
+			create_placeholder(content, data.target);
+			assign_iterator(content);
+			if (data.running && content.current_iterator.has_next()) {
+				setTimeout(function() {
+					content.placeholder.html(content.current_iterator.next());
+					animations.additive(data, content);
+				}, content.delay);
+			}
+		}
+	}
+
+	var render_strategies = {
+		'text-by-char': function(items) {
+			 return new char_iterator(items.toString());
+		},
+		'text-by-line': function(items) {
+			return new line_iterator(items.toString());
+		},
+		'text-one-shot': function(items) {
+			return new one_shot_iterator(items.toString());
+		},
+		iterator: function(items) {
+			return items.iterator();
 		}
 	}
 
@@ -104,7 +136,6 @@
 				while (data.running && data.current_content < data.content.length) {
 					content = data.content[data.current_content];
 					data.current_content++;
-					render_placeholder(content, data.target);
 					animations[content.animation](data, content);
 				}
 			});
@@ -123,7 +154,7 @@
 				data.running = false;
 				data.current_content = 0;
 				$.each(data.content, function(i, val) {
-					val.current_item = 0;
+					val.current_iterator = null;
 				});
 			});
 		},
